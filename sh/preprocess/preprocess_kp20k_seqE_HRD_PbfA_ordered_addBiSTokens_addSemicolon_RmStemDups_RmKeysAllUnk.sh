270e30af39db45d252e3039a48286de7e0146ec9
gasolsun <gasolsun@gmail.com>
2021-12-16 10:42:56 +0800

    update sh file

--- sh/preprocess/preprocess_kp20k_seqE_HRD_PbfA_ordered_addBiSTokens_addSemicolon_RmStemDups_RmKeysAllUnk.sh
+++ sh/preprocess/preprocess_kp20k_seqE_HRD_PbfA_ordered_addBiSTokens_addSemicolon_RmStemDups_RmKeysAllUnk.sh
@@ -2,8 +2,8 @@
 
 
 # change to the absolute path to ExHiRD-DKG
-home_dir="/research/king3/wchen/Code4Git/ExHiRD-DKG/"
-
+home_dir="."
+project_dir="../.."
 dataset="processed_kp20k"
 DATADIR="${home_dir}/data/train_valid_dataset"
 
@@ -14,17 +14,17 @@ data_type="seqE_HRD"
 unk_rm_type="RmKeysAllUnk"
 
 processed_type="PbfA_ordered_addBiSTokens_addSemicolon_RmStemDups_${unk_rm_type}"
-SAVED_DIR="${DATADIR}/onmt_processed_data/with_copy_${data_type}_${processed_type}"
+SAVED_DIR="${project_dir}/${DATADIR}/onmt_processed_dataset/with_copy_${data_type}_${processed_type}"
 mkdir -p ${SAVED_DIR}
 
 LOG_DIR="logs/preprocess/"
 mkdir -p ${LOG_DIR}
 
-/research/king3/wchen/Anaconda3/envs/py3.6_th1.0_cuda9.0/bin/python3.6 -u preprocess.py \
--train_src=${DATADIR}/processed_raw_data/${dataset}_training_context_filtered_${unk_rm_type}.txt \
--train_tgt=${DATADIR}/processed_raw_data/${dataset}_training_keyphrases_filtered_${processed_type}.txt \
--valid_src=${DATADIR}/processed_raw_data/${dataset}_validation_context_filtered_${unk_rm_type}.txt \
--valid_tgt=${DATADIR}/processed_raw_data/${dataset}_validation_keyphrases_filtered_${processed_type}.txt \
+../../venv/Scripts/python.exe -u ../../preprocess.py \
+-train_src=${project_dir}/${DATADIR}/processed_raw_data/${dataset}_training_context_filtered_${unk_rm_type}.txt \
+-train_tgt=${project_dir}/${DATADIR}/processed_raw_data/${dataset}_training_keyphrases_filtered_${processed_type}.txt \
+-valid_src=${project_dir}/${DATADIR}/processed_raw_data/${dataset}_validation_context_filtered_${unk_rm_type}.txt \
+-valid_tgt=${project_dir}/${DATADIR}/processed_raw_data/${dataset}_validation_keyphrases_filtered_${processed_type}.txt \
 -save_data=${SAVED_DIR}/full_${dataset} \
 -shard_size=20000 \
 -src_vocab_size=50000 \
